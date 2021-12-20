215ab46590ffddac5b3939ade271f5ae0953d3ad
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-20 14:29:46 +0700

    Many to Many

--- dist/role/role.controller.d.ts
+++ dist/role/role.controller.d.ts
@@ -3,8 +3,8 @@ export declare class RoleController {
     private roleService;
     constructor(roleService: RoleService);
     all(): Promise<import("./role.entity").Role[]>;
-    create(name: string): Promise<import("./role.entity").Role>;
+    create(name: string, ids: number[]): Promise<import("./role.entity").Role>;
     get(id: number): Promise<import("./role.entity").Role>;
-    update(id: number, name: string): Promise<import("./role.entity").Role>;
+    update(id: number, name: string, ids: number[]): Promise<import("./role.entity").Role>;
     delete(id: number): Promise<any>;
 }
