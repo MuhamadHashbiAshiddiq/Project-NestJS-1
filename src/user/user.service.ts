129421c7d91452551ba16b2c01b1451cd028e517
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-20 10:28:02 +0700

    Menambahkan pageinate pada file user.controller dan user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,26 +6,48 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
   async all(): Promise<User[]> {
     return this.userRepository.find();
   }
 
-  async create(data): Promise<User>{
+  async pageinate(page: number = 1): Promise<any> {
+    const take = 15;
+
+    const [users, total] = await this.userRepository.findAndCount({
+      take,
+      skip: (page - 1) * take,
+    });
+
+    return {
+      data: users.map((user) => {
+        const { password, ...data } = user;
+
+        return data;
+      }),
+      meta: {
+        total,
+        page,
+        last_page: Math.ceil(total / take),
+      },
+    };
+  }
+
+  async create(data): Promise<User> {
     return this.userRepository.save(data);
   }
 
-  async findOne(condition): Promise<User>{
+  async findOne(condition): Promise<User> {
     return this.userRepository.findOne(condition);
   }
 
-  async update(id: number, data): Promise<any>{
+  async update(id: number, data): Promise<any> {
     return this.userRepository.update(id, data);
   }
 
-  async delete(id: number): Promise<any>{
+  async delete(id: number): Promise<any> {
     return this.userRepository.delete(id);
   }
 }
