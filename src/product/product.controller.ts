caf4fc70bae3bbedbfdc830648e3be673e6145d1
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-21 11:12:36 +0700

    Menambahkan file product-update.dto dan mengedit product.controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { ProductCreateDto } from './models/product-create.dto';
+import { ProductUpdateDto } from './models/product-update.dto';
 import { ProductService } from './product.service';
 
 @UseGuards(AuthGuard)
@@ -14,7 +15,28 @@ export class ProductController {
   }
 
   @Post()
-  async create(@Body body: ProductCreateDto) {
+  async create(@Body() body: ProductCreateDto) {
     return this.productService.create(body);
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: number){
+    return this.productService.findOne({id});
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: ProductUpdateDto
+  ) {
+   
+    await this.productService.update(id, body);
+
+    return this.productService.findOne({id});
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.productService.delete(id);
   }
 }
