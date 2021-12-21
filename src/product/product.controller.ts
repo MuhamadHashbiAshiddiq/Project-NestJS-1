e13b81bd345a08d3dc91838a9e14799147c40f1e
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-21 10:36:58 +0700

    Menambahkan product

---
+++ src/product/product.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ProductCreateDto } from './models/product-create.dto';
+import { ProductService } from './product.service';
+
+@UseGuards(AuthGuard)
+@Controller('products')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async all(@Query('page') page = 1) {
+    return this.productService.paginate(page);
+  }
+
+  @Post()
+  async create(@Body body: ProductCreateDto) {
+    return this.productService.create(body);
+  }
+}
